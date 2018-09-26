27a1e90581d755c9895f176b241ada63544c5653
avaprin <sergey-gubkin@mail.ru>
2018-09-26 17:46:53 +0300

    qwe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function solveEquation(equation) {
+module.exports = function solveEquation(equation)
+{
+  let count = equation.length;
+  var A = [];
+  var Koef = [];
+  for(let i = 0;i<count;++i)
+  {
+    if ((equation.charAt(i) === "+" || equation.charAt(i) === '-'))
+    {
+      if (A.length)
+      {
+        Koef.push(Number(A.join("")));
+      }
+      A = [];
+      A.push(equation.charAt(i));
+    }
+    if (!isNaN(equation.charAt(i)) && equation.charAt(i) !== " ")
+    {
+      A.push(equation.charAt(i));
+    }
+    if (equation.charAt(i) === '^')
+    {
+      i++;
+    }
+  }
+  Koef.push(Number(A.join("")));
+  var a = Koef[0];
+  var b = Koef[1];
+  var c = Koef[2];
+  var d;
+    d=b*b - 4*a*c;
+    if(a>0)return [Math.round((-b-Math.sqrt(d))/(2*a)),Math.round((-b+Math.sqrt(d))/(2*a))].sort((c,d)=>c>=d);
+
+    else return [Math.round((-b+Math.sqrt(d))/(2*a)),Math.round((-b-Math.sqrt(d))/(2*a))].sort((c,d)=>c>=d);
   // your implementation
 }
